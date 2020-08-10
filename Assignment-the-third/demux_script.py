6c1169777eecfb461c3fd9c2205ea7ed409330d6
holston-a <aholston@uoregon.edu>
2020-08-10 15:59:07 -0700

    Updated unit test files and main demultiplexing script.

--- Assignment-the-third/demux_script.py
+++ Assignment-the-third/demux_script.py
@@ -33,18 +33,24 @@ def get_indexlist(indexfile):
                 indexlist.append(break_it_up[4])                                                #As long as the indexes are in the fifth column, we pull them out and add them to our list.
     return indexlist
 
-def rev_comp(indexlist):
+def rev_comp(sequence):
     """Given an index list, return a dictionary where the key is the index, and the value is the reverse complement of said index"""
-    RevDict = {'A':'T', 'G':'C', 'C':'G', 'T':'A', 'N':'N'}
+    RevDict = {'A':'T', 'G':'C', 'C':'G', 'T':'A', 'N':'N'}                 #Make our dictionary of canonical base pairs.
+    revseq = sequence[::-1]                                                 #Reverse our sequence.
+    revComplement = ''
+    for base in revseq:
+        matchbase = RevDict[base]
+        revComplement = revComplement + matchbase
+    return revComplement
+
+def rev_comp_maker(indexlist):
+    """Given an index list, return a dictionary where the key is the index, and the value is the reverse complement of said index"""
     rev_comp_dict = {}
     indexesfwdback = list(indexlist)
     for i in range(len(indexlist)):
-        rev_ele = indexlist[i][::-1]
-        rev_com = ''
-        for j in rev_ele:
-            rev_com+=str(RevDict[j])
-        indexesfwdback.append(rev_com)                                  #Make a list contains both the indexes and the reverse complement of the indexes.
-        rev_comp_dict[indexlist[i]] = rev_com                           #Generate a dictionary where the key is the index, and the value is the reverse complement of the index.
+        rc = rev_comp(indexlist[i])
+        rev_comp_dict[indexlist[i]] = rc                           #Generate a dictionary where the key is the index, and the value is the reverse complement of the index.
+        indexesfwdback.append(rc)                                  #Make a list contains both the indexes and the reverse complement of the indexes.
     return rev_comp_dict, indexesfwdback
 
 def convert_phred(letter):
@@ -71,6 +77,7 @@ def possible_perms(indexlist):
     permut_dict['Unk_lowQ'] = 0
     permut_dict['Index_hopped'] = 0
     permut_dict['Correctly_matched'] = 0
+    permut_dict['Total'] = 0
     return permut_dict
 
 
@@ -79,23 +86,23 @@ def open_write_files(indexlist):
     file_list = {}
     for i in range(len(indexlist)):                                              #Here, we are getting the names of our files.
         filename = indexlist[i] + "_Read1" + ".fastq"                #So we have nicely named files.
-        filehandle = open(filename,"a")                                    #'a' option is "append", so it creates files if they are not yet created, and opens them for appending otherwise.
+        filehandle = open(filename,"w")                                    #'a' option is "append", so it creates files if they are not yet created, and opens them for appending otherwise.
         file_list[filename] = filehandle                                   #Genericize this so we can more easily write to it later.
         filename = indexlist[i] + "_Read2" + ".fastq"                #So we have nicely named files for read 2.
-        filehandle = open(filename,"a")                                    #'a' option is "append", so it creates files if they are not yet created, and opens them for appending otherwise.
+        filehandle = open(filename,"w")                                    #'a' option is "append", so it creates files if they are not yet created, and opens them for appending otherwise.
         file_list[filename] = filehandle                                   #Once again, genericize this so we can more easily write to it later.
     #Now, this is great, but we still need to make files for our index hopping, 
         filename = "indexhopped_Read1.fastq"
-        filehandle = open(filename,"a")
+        filehandle = open(filename,"w")
         file_list[filename] = filehandle
         filename = "indexhopped_Read2.fastq"
-        filehandle = open(filename,"a")
+        filehandle = open(filename,"w")
         file_list[filename] = filehandle
         filename = "unk_lowqual_Read1.fastq"
-        filehandle = open(filename,"a")
+        filehandle = open(filename,"w")
         file_list[filename] = filehandle
         filename = "unk_lowqual_Read2.fastq"
-        filehandle = open(filename,"a")
+        filehandle = open(filename,"w")
         file_list[filename] = filehandle
     return file_list                                                       #Returns the file_list dictionary, which contains the names of all the files as the keys, and their filehandles as the values.
 
@@ -110,7 +117,7 @@ def close_written_files(file_list):
 def main_demultiplexing(read1,read2,index1,index2,indexfile):
     """Calls the output of other functions, demultiplexes the records, and all. Also tracks of index-hoppings, correctly paired, and low quality/unknown indexes."""
     indexlist = get_indexlist(indexfile)
-    rev_comp_dict, indexesfwdback = rev_comp(indexlist)
+    rev_comp_dict, indexesfwdback = rev_comp_maker(indexlist)     #POUHSLDGFJKDSF GONNA NEED TO CHANGE THIS, CUZ I'm CHANGING IT UP ABOVE!!! ! ! ! ! ! 
     permut_dict = possible_perms(indexlist)
     file_list = open_write_files(indexlist)                     #Open all of our files to which we'll write.
     with gzip.open(read1, "rt") as r1f, gzip.open(index1, "rt") as i1f, gzip.open(index2, "rt") as i2f, gzip.open(read2, "rt") as r2f:
@@ -130,7 +137,7 @@ def main_demultiplexing(read1,read2,index1,index2,indexfile):
             recordHolder[2].append(i2fLine)
             recordHolder[3].append(r2fLine)
 
-            if LN % 4 == 0:
+            if LN % 4 == 0:                                                     #So we have a full record
                 indices2header = str(recordHolder[1][1]) + "-" + str(rev_comp(recordHolder[2][1]))
                 recordHolder[0][0] = str(recordHolder[0][0]) + ":" + indices2header
                 recordHolder[3][0] = str(recordHolder[3][0]) + ":" + indices2header
@@ -139,45 +146,53 @@ def main_demultiplexing(read1,read2,index1,index2,indexfile):
                 i1 = RecordHolder[1][1]
                 i2 = RecordHolder[2][1]
 
-                if i1 not in indexlist or i2 not in indexesfwdback or 'N' in i1 or 'N' in i2:
+                i1qual = RecordHolder[1][3]
+                i2qual = RecordHolder[2][3]
+                if i1 in indexlist and i2 in indexesfwdback:
+                    if i2 == rev_comp_dict[i1] and avg_qscore(i1qual) >= 30 and avg_qscore(i2qual) >= 30:                             #So, if the second index is the reverse complement of the first index,
+                        permut_dict[(i1,i1)]+= 1                                             #Update our counter, so we can keep track of the pairs
+                        permut_dict['Correctly_matched']+= 1                                 #And of our overall correct matches.
+                        permut_dict['Total']+= 1
+                        for i in range(len(RecordHolder[3])):                                #Iterate through the lines of the record, because as we are no longer manipulating the information, we can now...
+                            RecordHolder[3][i] = RecordHolder[3][i] +'\n'                    #...add newlines to the lines so we can write the records to a file
+                            RecordHolder[0][i] = RecordHolder[0][i] +'\n'
+                        named_file1 = i1 + '_Read1.fastq'                                       #Then we find the name of the file for the first one,
+                        file_list[named_file1].writelines(RecordHolder[0])                   #And write the read1 record to the appropriate file.
+                        named_file2 = i1 + '_Read2.fastq'                                       #Similarly, we find the file name for the second one,
+                        file_list[named_file2].writelines(RecordHolder[3])                   #And write to that file.
+                    elif avg_qscore(i1qual) >= 30 and avg_qscore(i2qual) >= 30:
+                        i1str = str(i1)
+                        i2rc = str(rev_comp(i2))                                                  #Now we get the reverse complement of the 2nd index, so we have what would initially be its forward sequence
+                        permut_dict[(i1str,i2rc)]+= 1                                           #And increment its specific counter
+                        permut_dict['Index_hopped']+= 1                                        #And increment its generic counter
+                        permut_dict['Total']+= 1
+                        for i in range(len(RecordHolder[3])):                                #Iterate through the lines of the record, because as we are no longer manipulating the information, we can now...
+                            RecordHolder[3][i] = RecordHolder[3][i] +'\n'                    #...add newlines to the lines so we can write the records to a file
+                            RecordHolder[0][i] = RecordHolder[0][i] +'\n'
+                        file_list['indexhopped_Read1.fastq'].writelines(RecordHolder[0])     #Then we write the records to the files
+                        file_list['indexhopped_Read2.fastq'].writelines(RecordHolder[3])
+                    else:
+                        permut_dict['Unk_lowQ']+= 1                                            #Update our counter, so we can keep track of the low quality or unknown records
+                        permut_dict['Total']+= 1
+                        for i in range(len(RecordHolder[3])):                                #Iterate through the lines of the record, because as we are no longer manipulating the information, we can now...
+                            RecordHolder[3][i] = RecordHolder[3][i] +'\n'                    #...add newlines to the lines so we can write the records to a file
+                            RecordHolder[0][i] = RecordHolder[0][i] +'\n'
+                        file_list['unk_lowqual_Read1.fastq'].writelines(RecordHolder[0])     #Actually write them to files.
+                        file_list['unk_lowqual_Read2.fastq'].writelines(RecordHolder[3])
+                else:
                     permut_dict['Unk_lowQ']+= 1                                            #Update our counter, so we can keep track of the low quality or unknown records
+                    permut_dict['Total']+= 1
                     for i in range(len(RecordHolder[3])):                                #Iterate through the lines of the record, because as we are no longer manipulating the information, we can now...
                         RecordHolder[3][i] = RecordHolder[3][i] +'\n'                    #...add newlines to the lines so we can write the records to a file
                         RecordHolder[0][i] = RecordHolder[0][i] +'\n'
                     file_list['unk_lowqual_Read1.fastq'].writelines(RecordHolder[0])     #Actually write them to files.
                     file_list['unk_lowqual_Read2.fastq'].writelines(RecordHolder[3])
-                elif avg_qscore(RecordHolder[1][3]) < 30 or avg_qscore(RecordHolder[2][3]) < 30:
-                    permut_dict['Unk_lowQ']+= 1                                            #Update our counter, so we can keep track of the low quality or unknown records
-                    for i in range(len(RecordHolder[3])):                                #Iterate through the lines of the record, because as we are no longer manipulating the information, we can now...
-                        RecordHolder[3][i] = RecordHolder[3][i] +'\n'                    #...add newlines to the lines so we can write the records to a file
-                        RecordHolder[0][i] = RecordHolder[0][i] +'\n'
-                    file_list['unk_lowqual_Read1.fastq'].writelines(RecordHolder[0])     #Actually write them to files.
-                    file_list['unk_lowqual_Read2.fastq'].writelines(RecordHolder[3])
-                elif rev_comp_dict[i1] == i2:                                             #So, if the second index is the reverse complement of the first index,
-                    permut_dict[(i1,i1)]+= 1                                             #Update our counter, so we can keep track of the pairs
-                    permut_dict['Correctly_matched']+= 1                                   #And of our overall correct matches.
-                    for i in range(len(RecordHolder[3])):                                #Iterate through the lines of the record, because as we are no longer manipulating the information, we can now...
-                        RecordHolder[3][i] = RecordHolder[3][i] +'\n'                    #...add newlines to the lines so we can write the records to a file
-                        RecordHolder[0][i] = RecordHolder[0][i] +'\n'
-                    named_file1 = i1 + '_Read1.fastq'                                       #Then we find the name of the file for the first one,
-                    file_list[named_file1].writelines(RecordHolder[0])                   #And write the read1 record to the appropriate file.
-                    named_file2 = i1 + '_Read2.fastq'                                       #Similarly, we find the file name for the second one,
-                    file_list[named_file2].writelines(RecordHolder[3])                   #And write to that file.
-                else:                                                                    #As we have now taken out all of the records where the indexes weren't in our index list, and also the ones where they were in our index and matched properly, then...
-                    i2rc = rev_comp(i2)                                                  #Now we get the reverse complement of the 2nd index, so we have what would initially be its forward sequence
-                    permut_dict[(i1,i2rc)]+= 1                                           #And increment its specific counter
-                    permut_dict['Index_hopped']+= 1                                        #And increment its generic counter
-                    for i in range(len(RecordHolder[3])):                                #Iterate through the lines of the record, because as we are no longer manipulating the information, we can now...
-                        RecordHolder[3][i] = RecordHolder[3][i] +'\n'                    #...add newlines to the lines so we can write the records to a file
-                        RecordHolder[0][i] = RecordHolder[0][i] +'\n'
-                    file_list['indexhopped_Read1.fastq'].writelines(RecordHolder[0])     #Then we write the records to the files
-                    file_list['indexhopped_Read2.fastq'].writelines(RecordHolder[3])
-            RecordHolder = [[],[],[],[]]                                                 #Reset our record holder to zero so that it will be ready for the next set of records given it
+                    
+                recordHolder = [[],[],[],[]]                                                 #Reset our record holder to zero so that it will be ready for the next set of records given it
     close_written_files(file_list)                                                       #Now we close all our files have opened
     print('Entity'+'\t'+'Times encountered')
     for key,value in permut_dict.items():
         print(key,'\t',value)
     return "Successfully demultiplexed"
-
 
 main_demultiplexing(read1,read2,index1,index2,indexfile)                                 #Now we actually do it! 
